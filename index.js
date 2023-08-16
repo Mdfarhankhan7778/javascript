c19f8e1429096178160ecc5623ea9e20f16c5de2
Mdfarhankhan7778 <farhanraza2001@gmail.com>
2023-08-16 15:49:46 +0530

    second commit

--- index.js
+++ index.js
@@ -41,12 +41,47 @@ console.log(f);
 // single quotes, double quotes, backtick
 var str ='a';
 console.log(typeof str);
-str="how you done";
+str="how you done ?\ni am farhan khan ";
+//backslash n("\n")
 console.log(str);
 
+var b=`hi hope you guys are
+able grasp the concept`; // here we donot need of \n
+console.log(b);
+
+var num= 300;
+console.log(`half of ${num} is ${num/2}`)
+
+//var is re declaratio. to overcome that is
+// let came
+// let keyword is used
+let l=100;
+console.log(l);
+
+ //let l= "200"; // syntax error : identifier 'l' has already been declared
+
+// we can reinitialize a variable but we cannot redeclare it
+l=300;
+console.log(l);
+
+// const 
+const a =2;
+console.log(a);
+//na hi redeclaration na reinitializatin
+//const a= 5;
+//a=4;   typeerror
+// console.log("hey");
 
 
+//loop in js
+var num = 10;
+for(var i=0; i<num;
+    i++){
+        if(i%2==0){
+            console.log("num is even")
+        }
 
+    }
 
 
 
