44a4595499c22c71f2798af5b317231453811b7c
Mdfarhankhan7778 <farhanraza2001@gmail.com>
2023-08-16 12:57:59 +0530

    js data types done

---
+++ index.js
@@ -0,0 +1,64 @@
+
+// starting js
+// var let const
+
+// varible declaration
+
+/*  a=10;
+console.log(a);
+console.log(typeof a);
+
+a="hello"; // re initialize of variable
+console.log(a);
+console.log(typeof a);
+
+// typeof operator-> it tells us the type of data stored in a variable
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a); */
+
+
+// number
+var num= 10;
+console.log(num);
+var float = 2.4;
+console.log(float);
+
+
+//boolean
+var t = true;
+var f = false;
+console.log(t);
+console.log(f);
+
+//string
+// '',"",``
+// single quotes, double quotes, backtick
+var str ='a';
+console.log(typeof str);
+str="how you done";
+console.log(str);
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
