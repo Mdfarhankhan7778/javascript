72c64551ea471bcf3595911808616af7e6a096a8
Mdfarhankhan7778 <farhanraza2001@gmail.com>
2023-09-07 14:20:22 +0530

    node js

---
+++ Node/fs.js
@@ -0,0 +1,16 @@
+// fs-> file system module. it helps us 
+
+const fs = require("fs");  // require(" path of the file")
+// appendFileSync appends data into a file, if file is not present , it creates the file and then appends the data
+let res=fs.appendFileSync("f1.txt","hello i am f2 file");
+fs.appendFileSync("f1.txt","\nyou guys are smart");
+// console.log(res);
+
+let data = fs.readFileSync("f1.txt","utf-8"); // utf is like a encoding , it decodes to encodes
+// data received is an object type,data is in buffer 
+console.log(data);
+
+// const f1 = require("'/f1.txt");
+//const abc= require("../js/temp");
+// require method , goes to the file that is needed to be required .executes that file.
+// console.log(abc);
