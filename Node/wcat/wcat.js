e1322d3c4ffeeeae5603250b77e68f280818e021
Mdfarhankhan7778 <farhanraza2001@gmail.com>
2023-09-11 11:55:10 +0530

    wcat completed

--- Node/wcat/wcat.js
+++ Node/wcat/wcat.js
@@ -1,20 +1,22 @@
 // 1)node wcat.js filepath => display the contents of a file in terminal
-// node wcat.js filepath 1 filepath2 filepath3 => display the content of all files in terminal in concatination
+// 2)node wcat.js filepath 1 filepath2 filepath3 => display the content of all files in terminal in concatination form in given order
+//3) node wcat.js -n file1 file2 file3 or node wcat.js -n file1
 //node wcat.js f1.txt
 // node wcat.js f1.txt f2.txt f3.txt
 const fs = require("fs");
 let inputArr = process.argv.slice(2);
-//console.log(input);
+console.log(inputArr);
 
 let filesArr = [];
-// placed files path in fileArr
+let optionsArr = [];
+// -------------------- placed files path in fileArr -----------------------
 for(let i = 0; i<inputArr.length;i++){
-    filesArr.push(inputArr[i]);
+    filesArr.push(inputArr[i].charAt(0));
 
 }
 // console.log("file to be read are "+filesArr);
 
-//check is all the files are present
+//==============================check is all the files are present
 
 for(let i = 0; i<filesArr.length; i++){
     let doesExist = fs.existsSync(filesArr[i]);
@@ -24,11 +26,89 @@ for(let i = 0; i<filesArr.length; i++){
     }
 }
 
-// content read and appending starts
+// ======================content read and appending starts
 let content = "";
 for(let i=0; i<filesArr.length; i++){
     let fileContent = fs.readFileSync(filesArr[i]);
-    content+= fileContent+"\n";
+    content=content + fileContent+"\r\n";
 
 }
 console.log(content);
+
+let contentArr = content.split("\r\n");
+console.table(contentArr);
+
+// check -s is present or not
+let isSpresent= optionsArr.includes("-s");
+if(isSpresent){
+    for(let i=1;i<contentArr.length;i++){
+        if(contentArr[i]=="" && contentArr[i-1] == ""){
+            contentArr[i] = null;
+        }
+
+    }
+    console.table(contentArr);
+    let tempArr = [];
+    // push everything in tempArr except null
+for(let i=0;i<contentArr.length;i++){
+    if(contentArr[i]!=null){
+        tempArr.push(contentArr[i]);
+
+    }
+}
+console.log("data after removing extra lines\n",tempArr);
+}
+contentArr = tempArr;
+
+let indexOfN = optionsArr.indexOf("-n");
+let indexOfB = optionsArr.indexOf("-b");
+// if 
+
+let finalOption = "";
+// if both -n and -b are present
+if(indexOfN!=-1 && indexOfB!=-1){
+
+if(indexOfN<indexOfB){
+    finalOption="-n";
+
+}
+else {
+    finalOption = "-b";
+
+}
+}
+
+// either -n is present or -b is present
+else {
+    if (indexOfN != -1){
+        finalOption = "-n";
+    }
+    else if(indexOfN != -1) {
+        finalOption="-b";
+    }
+}
+
+// calling of function by evaluating finalOption
+
+if(finalOption == "-n"){
+    modifiyContentByN();
+}
+else if (finalOption == "-b"){
+    modifiyContentByB();
+}
+
+function modifiyContentByN(){
+    for(let i = 0; i<contentArr.length;i++){
+         contentArr[i] = (i+1) + ")" + contentArr[i];
+    }
+}
+
+function modifiyContentByB(){
+    let count = 1;
+    for(let i = 0; i<contentArr.length;i++){
+        if (contentArr[i] !=""){
+        contentArr[i] = count +")" + contentArr[i];
+        count ++;
+    }   
+}
+}
